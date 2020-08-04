969369d57335380979edb851f180bb8d052f9f1a
Eduardo Basso <eduardo.basso@br.ey.com>
2020-08-04 11:40:16 -0300

    chore: testing router build (3)

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve("dist"),
-    filename: "bundle.js",
+    filename: "index_bundle.js",
     publicPath: '/'
   },
   module: {
