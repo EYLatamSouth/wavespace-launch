db77ebb707cc5b477372fd14f8314ad5615d9548
Eduardo Basso <eduardo.basso@br.ey.com>
2020-08-04 11:13:20 -0300

    chore: testing router build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const webpack = require('webpack');
+const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   output: {
-    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
     publicPath: '/'
   },
   module: {
